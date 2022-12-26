0149c64984ff50d42eb30d38d97c71dd5d946d90
patelpreet1208 <patelpreet1208@gmail.com>
2022-12-26 19:01:05 +0530

    26-12-2022

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-    test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-    use: [
-        {
-            loader: 'babel-loader',
-        },
-        {
-            loader: '@svgr/webpack',
-            options: {
-                babel: false,
-                icon: true,
+    mode: 'development',
+    entry: {
+        polyfills: './src/polyfills.ts',
+        index: './src/index.tsx',
+    },
+    output: {
+        path: path.resolve(__dirname, './output'),
+        filename: '[id].bundle.js',
+    },
+    devtool: 'cheap-module-source-map',
+    resolve: {
+        extensions: ['.ts', '.js', '.tsx', '.json'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(t|j)sx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'ts-loader',
+                    options: {
+                        transpileOnly: true,
+                    },
+                },
             },
-        },
+            {
+                test: /\.html$/,
+                exclude: /node_modules/,
+                use: { loader: 'html-loader' },
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index-webpack.html',
+        }),
     ],
-}
+    devServer: {
+        host: '127.0.0.1',
+        port: 8080,
+    },
+    stats: {
+        children: true,
+    },
+};
